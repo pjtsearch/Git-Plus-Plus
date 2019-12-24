3bad38a20b7a301047b93184b3db17aafb6147bc
PJTSearch <pjtsignups@gmail.com>
2019-12-24 07:51:37 -0800

    make push have dropdowns instead of inputs

--- push.js
+++ push.js
@@ -5,13 +5,21 @@ module.exports = {
 		const git = simpleGit(graviton.getCurrentDirectory());
 
 		const status = await git.status();
+		const remotes = (await git.getRemotes()).map(remote=>remote.name)
+		const branches = (await git.branch()).all
 
 		let dialog = new Dialog({
 			id: "git-plus-plus-status-dialog",
 			title: "Git Push",
 			content: `
-<input class="input4" id="push-remote" placeHolder="Remote"></input>
-<input class="input4" id="push-branch" placeHolder="Branch"></input>
+<!--<input class="input4" id="push-remote" placeHolder="Remote"></input>-->
+<select id="push-remote">
+${remotes.map(remote=>`<option>${remote}</option>`).join("\n")}
+</select>
+<!--<input class="input4" id="push-branch" placeHolder="Branch"></input>-->
+<select id="push-branch">
+${branches.map(branch=>`<option>${branch}</option>`).join("\n")}
+</select>
 `,
 			buttons: {
 				"Push": {click:async()=>{
