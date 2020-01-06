0913f6bfad578821646e5f4a55580482dd18b61f
PJTSearch <pjtsignups@gmail.com>
2020-01-06 11:45:55 -0800

    get rid of babel imort in rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,5 @@
 import resolve from '@rollup/plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
-//import postcss from 'rollup-plugin-postcss'
-
+//import babel from 'rollup-plugin-babel';
 
 export default {
   input: 'src/main.js',
